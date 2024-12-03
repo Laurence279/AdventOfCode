5fc2261625d13c364be4ca07d7f44d786096717d
Laurence Nunn <lnunn279@gmail.com>
2024-12-03 20:23:02 +0000

    Tidy up registry to be more dynamic

--- AdventOfCode.CLI/Days/DayRegistry.cs
+++ AdventOfCode.CLI/Days/DayRegistry.cs
@@ -12,14 +12,46 @@
 
         private void Setup()
         {
-            this.registry.Add(1, new Day1());
-            this.registry.Add(2, new Day2());
-            this.registry.Add(3, new Day3());
+            var days = new Dictionary<int, Type>()
+            {
+                { 1, typeof(Day1) },
+                { 2, typeof(Day2) },
+                { 3, typeof(Day3) }
+            };
+
+            foreach (var day in days)
+            {
+                var dayInstance = Activator.CreateInstance(day.Value, this.GetInput(day.Key)) as Puzzle;
+                if (dayInstance != null)
+                {
+                    this.registry.Add(day.Key, dayInstance);
+                }
+            }
+        }
+
+        private string GetInput(int day)
+        {
+            var path = $"./Days/day{day}.txt";
+            if (File.Exists(path))
+            {
+                return File.ReadAllText(path);
+            }
+            else
+            {
+                throw new FileNotFoundException($"{path} does not exist.");
+            }
         }
 
         public Puzzle GetDay(int day)
         {
-            return this.registry[day];
+            if (this.registry.ContainsKey(day))
+            {
+                return this.registry[day];
+            }
+            else
+            {
+                throw new KeyNotFoundException($"Day {day} does not exist.");
+            }
         }
     }
 }
